60f1f6e896b91eddfedffad1935b1383ae8dd7af
manal1225 <72064207+manal1225@users.noreply.github.com>
2022-11-18 21:22:56 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCqmOIhtrhtmABpQMZrt1Px-M89qiHzJ5A",
+  authDomain: "storytelling-5d7ed.firebaseapp.com",
+  databaseURL: "https://storytelling-5d7ed-default-rtdb.firebaseio.com",
+  projectId: "storytelling-5d7ed",
+  storageBucket: "storytelling-5d7ed.appspot.com",
+  messagingSenderId: "737851481282",
+  appId: "1:737851481282:web:62b557ecdeabd5d940ef5b"
+};
